031f133d63ff9fccbb799199752dc500fff72157
Leandro Herbas <leandro.herbas@lactec.com.br>
2026-01-21 14:05:07 -0300

    Exercício 43: Fazer o log de todos os eventos disparados pelo backend

---
+++ backend/src/lactec/intranet/subscribers/logger.py
@@ -0,0 +1,21 @@
+from lactec.intranet import logger
+
+import os
+
+def log_events(event: object):
+    """Escreve no log todos os eventos disparados pelo processo do backend
+    se a variável de ambiente DEBUG estiver definida.
+    """
+    if not os.environ.get("DEBUG"):
+        return
+
+    # Caminho do módulo que disparou o evento
+    module_name = event.__class__.__module__
+
+    # Classe que disparou o evento
+    class_name = event.__class__.__name__
+
+    # Caminho completo da classe que disparou o evento
+    dotted_name = f"{module_name}.{class_name}"
+
+    logger.info(f"\nEvento disparado: {dotted_name} ({event})\n")
